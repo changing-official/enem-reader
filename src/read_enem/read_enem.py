2a4d5df3162c22efab9fbe20974d73d9c6b23209
loelias <loe_elias@msn.com>
2018-06-01 19:56:07 -0300

    add test read enem file

---
+++ src/read_enem/read_enem.py
@@ -0,0 +1,12 @@
+class ReaderCSV():
+    def read(self, file):
+        return True
+
+    def output(self):
+        # install pandas
+
+        #call pandas reading csv file
+
+        #return csvfile in string[] format
+
+        return ""
